3c6d275dba9373f339434a591f1dcf53a92b73de
Yurij <yura@intech.kiev.ua>
2015-08-27 13:39:53 +0300

    Update dns.inc.php

--- dns.inc.php
+++ dns.inc.php
@@ -404,6 +404,15 @@ class DNSQuery
 				
 				$string=$domain." TEXT \"".$data . "\" (in " . $string_count . " strings)";
 				break;
+				
+			case "NAPTR":
+				$data = "";
+				$stuff = $this->ReadResponse(4);
+				$extras = unpack("norder/npreference",$stuff);
+				$data .= $this->ReadDomainLabel();
+				$data .= $this->ReadDomainLabel();
+				$string = $domain." NAPTR ".$data;
+				break;
 
 			default: // something we can't deal with
 				$stuff=$this->ReadResponse($ans_header['length']);
